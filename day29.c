8d8cf10d08a4657dc16be18afe2f7f7c1646a68a
PawniMadaan <pawnimadaan11@gmail.com>
2026-03-01 23:36:54 +0530

    Twenty nineth commit

---
+++ day29.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void insert(struct Node** head, int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+struct Node* rotateRight(struct Node* head, int k) {
+    if (head == NULL || head->next == NULL || k == 0)
+        return head;
+
+    struct Node* temp = head;
+    int length = 1;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+        length++;
+    }
+
+    temp->next = head;
+
+    k = k % length;
+    int stepsToNewHead = length - k;
+
+    struct Node* newTail = head;
+    for (int i = 1; i < stepsToNewHead; i++) {
+        newTail = newTail->next;
+    }
+
+    struct Node* newHead = newTail->next;
+    newTail->next = NULL;
+
+    return newHead;
+}
+
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* head = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+        insert(&head, value);
+    }
+
+    int k;
+    scanf("%d", &k);
+
+    head = rotateRight(head, k);
+
+    printList(head);
+
+    return 0;
+}
